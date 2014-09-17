b01b04c24333f784f9fe3668be793044596fa269
Gawaine O'Gilvie <gawaine.ogilvie@gmail.com>
2014-09-17 16:14:04 -0400

    height and weight entries are complete.

--- gui/bmi_calculator.py
+++ gui/bmi_calculator.py
@@ -5,3 +5,35 @@ Project: BMI Calculator
 Date Started: Sept. 17, 2014
 Author: Gawaine O'Gilvie
 """
+
+import Tkinter
+
+class bmi_calc(Tkinter.Tk):
+
+	def __init__(self, parent):
+		
+		Tkinter.Tk.__init__(self, parent)
+		self.parent = parent
+		self.initialize()
+
+	def initialize(self):
+		
+		self.grid()
+		#self.label_standard_vs_metric
+		self.label_height = Tkinter.Label(self, text="Your Height:")
+		self.label_height.grid(column=0,row=1, pady=8)
+
+		self.label_weight = Tkinter.Label(self, text="Your Weight:")
+		self.label_weight.grid(column=0,row=2, pady=8)
+
+		self.entry_height = Tkinter.Entry(self)
+		self.entry_height.grid(column=2,row=1,pady=8)
+
+		self.entry_weight = Tkinter.Entry(self)
+		self.entry_weight.grid(column=2,row=2,pady=8)
+
+
+if __name__ == "__main__":
+	app = bmi_calc(None)
+	app.title('BMI Calculator')
+	app.mainloop()
