9b1a05c97481772c7b808e74d350e6784f78f126
Gawaine O'Gilvie <gawaine.ogilvie@gmail.com>
2014-09-28 18:06:03 -0400

    able to calculate the metric bmi

--- gui/bmi_calculator.py
+++ gui/bmi_calculator.py
@@ -22,15 +22,17 @@ class bmi_calc(Tkinter.Tk):
 		self.geometry('400x300')
 
 		#self.label_standard_vs_metric
+		# unit = self.IntVar()
+		# unit.set(2)
 
 		self.standard_unit = Tkinter.Radiobutton(self, text="Standard (US):", value=1)
 		self.standard_unit.grid(column=0, row=0, pady=5, padx=5, sticky="W")
 
-		self.metric_unit = Tkinter.Radiobutton(self, text="Metric", value=2)
+		self.metric_unit = Tkinter.Radiobutton(self, text="Metric",value=2)
 		self.metric_unit.grid(column=2, row=0, pady=5, padx=5, sticky="W")
 
 
-		self.label_height = Tkinter.Label(self, text="Your Height (centimeters):")
+		self.label_height = Tkinter.Label(self, text="Your Height (meters):")
 		self.label_height.grid(column=0,row=1, pady=5, padx=5, sticky="W")
 
 		self.label_weight = Tkinter.Label(self, text="Your Weight (kilograms):")
@@ -43,14 +45,35 @@ class bmi_calc(Tkinter.Tk):
 		self.entry_height.grid(column=2,row=1,pady=5, padx=5, sticky="W")
 
 		self.entry_weight = Tkinter.Entry(self)
-		self.entry_weight.grid(column=2,row=2,pady=5, padx=5, sticky="W")
+		self.entry_weight.grid(column=2,row=2, pady=5, padx=5, sticky="W")
 
-		self.entry_bmi = Tkinter.Entry(self)
+		self.compute_bmi = Tkinter.Button(self, text="Calculate BMI", command=self.calcBMI)
+		self.compute_bmi.grid(column=2, row=4, pady=5, padx=5)
+		#self.compute_bmi.bind("<Button-1>", self.calcBMI)
+
+		self.entryVariable = Tkinter.StringVar()
+
+		self.entry_bmi = Tkinter.Entry(self, textvariable=self.entryVariable)
 		self.entry_bmi.grid(column=2,row=10, pady=5, padx=5, sticky="W")
+		self.entry_bmi.bind("<Button-1>", self.calcBMI)
+		self.entryVariable.set(u"Your BMI is here.")
+		
 
 		#prevent the main window from resizing
 		self.resizable(False, False)
 
+	def calcBMI(self):
+
+		bmi = float(self.entry_weight.get())/(float(self.entry_height.get()) ** 2)
+
+		if bmi < 18.5:
+			self.entryVariable.set("BMI = %d = Underweight" % (bmi))
+		elif bmi >= 18.5 and bmi <= 24.9:
+			self.entryVariable.set("BMI = %d = Normal weight" % (bmi))
+		elif bmi >= 25 and bmi <= 29.9:
+			self.entryVariable.set("BMI = %d = Overweight" % (bmi))
+		else:
+			self.entryVariable.set("BMI = %d = Obesity" % (bmi))
 
 
 
