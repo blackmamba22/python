0f3b539da10530273d6ad4c0112eee95c703a5d0
Gawaine O'Gilvie <gawaine.ogilvie@gmail.com>
2014-09-29 20:20:15 -0400

    Standard units now work and has the appropriate labeling

--- gui/bmi_calculator.py
+++ gui/bmi_calculator.py
@@ -21,16 +21,53 @@ class bmi_calc(Tkinter.Tk):
 		self.grid()
 		self.geometry('350x250')
 
-		#self.label_standard_vs_metric
-		# unit = self.IntVar()
-		# unit.set(2)
+		self.unit = Tkinter.IntVar()
+		self.unit.set(2)			#default value set to Metric System
 
-		self.standard_unit = Tkinter.Radiobutton(self, text="Standard (US):", value=1)
+		self.useMetric() # initialize metric fields
+
+		self.standard_unit = Tkinter.Radiobutton(self, text="Standard (US):", variable=self.unit, value=1, command=self.useStandard)
 		self.standard_unit.grid(column=0, row=0, pady=5, padx=5, sticky="W")
+		self.standard_unit.bind("<Button-1>", self.useStandard())
 
-		self.metric_unit = Tkinter.Radiobutton(self, text="Metric",value=2)
+		self.metric_unit = Tkinter.Radiobutton(self, text="Metric", variable=self.unit, value=2, command=self.useMetric)
 		self.metric_unit.grid(column=2, row=0, pady=5, padx=5, sticky="W")
+		self.metric_unit.bind("<Button-1>", self.useMetric())
 
+
+		# self.label_height = Tkinter.Label(self, text="Your Height (meters):")
+		# self.label_height.grid(column=0,row=1, pady=5, padx=5, sticky="W")
+
+		# self.label_weight = Tkinter.Label(self, text="Your Weight (kilograms):")
+		# self.label_weight.grid(column=0,row=2, pady=5, padx=5, sticky="W")
+
+		# self.label_bmi = Tkinter.Label(self, text="Your BMI:")
+		# self.label_bmi.grid(column=0,row=10, pady=5, padx=5, sticky="W")
+
+		# self.entry_height = Tkinter.Entry(self)
+		# self.entry_height.grid(column=2,row=1,pady=5, padx=5, sticky="W")
+
+		# self.entry_weight = Tkinter.Entry(self)
+		# self.entry_weight.grid(column=2,row=2, pady=5, padx=5, sticky="W")
+
+		self.compute_bmi = Tkinter.Button(self, text="Calculate BMI", command=self.calcBMI)
+		self.compute_bmi.grid(column=2, row=4, pady=5, padx=5)
+
+		self.entryVariable = Tkinter.StringVar()
+
+		self.entry_bmi = Tkinter.Entry(self, textvariable=self.entryVariable)
+		self.entry_bmi.grid(column=2,row=10, pady=5, padx=5, sticky="W")
+		self.entry_bmi.bind("<Button-1>", self.calcBMI)
+		self.entryVariable.set(u"Your BMI is here.")
+
+		self.quit_app = Tkinter.Button(self, text="Quit", command=self.quit)
+		self.quit_app.grid(column=2, row=12, pady=5, padx=5, sticky="W")
+		
+
+		#prevent the main window from resizing
+		self.resizable(False, False)
+
+	def useMetric(self):
 
 		self.label_height = Tkinter.Label(self, text="Your Height (meters):")
 		self.label_height.grid(column=0,row=1, pady=5, padx=5, sticky="W")
@@ -47,27 +84,36 @@ class bmi_calc(Tkinter.Tk):
 		self.entry_weight = Tkinter.Entry(self)
 		self.entry_weight.grid(column=2,row=2, pady=5, padx=5, sticky="W")
 
-		self.compute_bmi = Tkinter.Button(self, text="Calculate BMI", command=self.calcBMI)
-		self.compute_bmi.grid(column=2, row=4, pady=5, padx=5)
-		#self.compute_bmi.bind("<Button-1>", self.calcBMI)
+	def useStandard(self):
 
-		self.entryVariable = Tkinter.StringVar()
+		self.label_height = Tkinter.Label(self, text="Your Height (inches):")
+		self.label_height.grid(column=0,row=1, pady=5, padx=5, sticky="W")
 
-		self.entry_bmi = Tkinter.Entry(self, textvariable=self.entryVariable)
-		self.entry_bmi.grid(column=2,row=10, pady=5, padx=5, sticky="W")
-		self.entry_bmi.bind("<Button-1>", self.calcBMI)
-		self.entryVariable.set(u"Your BMI is here.")
+		self.label_weight = Tkinter.Label(self, text="Your Weight (lbs):")
+		self.label_weight.grid(column=0,row=2, pady=5, padx=5, sticky="W")
 
-		self.quit_app = Tkinter.Button(self, text="Quit", command=self.quit)
-		self.quit_app.grid(column=2, row=12, pady=5, padx=5, sticky="W")
-		
+		self.label_bmi = Tkinter.Label(self, text="Your BMI:")
+		self.label_bmi.grid(column=0,row=10, pady=5, padx=5, sticky="W")
 
-		#prevent the main window from resizing
-		self.resizable(False, False)
+		self.entry_height = Tkinter.Entry(self)
+		self.entry_height.grid(column=2,row=1,pady=5, padx=5, sticky="W")
+
+		self.entry_weight = Tkinter.Entry(self)
+		self.entry_weight.grid(column=2,row=2, pady=5, padx=5, sticky="W")
+
+
 
 	def calcBMI(self):
 
-		bmi = float(self.entry_weight.get())/(float(self.entry_height.get()) ** 2)
+		bmi = None
+
+		if self.unit.get() == 1:
+			print "Standard works"
+			bmi = float(self.entry_weight.get())/(float(self.entry_height.get()) ** 2) * 703
+		elif self.unit.get() == 2:
+			bmi = float(self.entry_weight.get())/(float(self.entry_height.get()) ** 2)
+
+		#bmi = float(self.entry_weight.get())/(float(self.entry_height.get()) ** 2)
 
 		if bmi < 18.5:
 			self.entryVariable.set("BMI = %d = Underweight" % (bmi))
