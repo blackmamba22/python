3093ffbb138413c9243bf046474819a6497c6a3a
unknown <Gawaine_O'Gilvie@CMA-GOgilvie.nuance.com>
2016-04-11 14:28:21 -0400

    updated exceptions and return strategy for richter_scale function

--- python3/software-eng-concepts/projects/FirstSteps.py
+++ python3/software-eng-concepts/projects/FirstSteps.py
@@ -1,4 +1,4 @@
-
+import sys
 
 class FirstSteps:
     """Basic python programs to get started."""
@@ -7,22 +7,28 @@ class FirstSteps:
         pass
 
     @staticmethod
-    def richter_scale():
+    def richter_scale(r_value=None):
         """
         Prompt user to enter a richter scale # and perform calculations to
         display equivalent amount of energy in joules and in tons of exploded TNT.
         """
-        user_input = input("Enter a richter scale value: ")
-        energy_in_joules = -1
-        num_tons_tnt_exploded = -1
+
+        if r_value is None:
+            user_input = input("Enter a richter scale value: ")
+        else:
+            user_input = r_value
+
+        energy_in_joules = None
+        num_tons_tnt_exploded = None
 
         try:
             richter_scale_val = float(user_input)
             energy_in_joules = 10**((1.5 * richter_scale_val) + 4.8)
             num_tons_tnt_exploded = energy_in_joules / (4.184 * 10**9)
-        except Exception as e:
-            print (e)
-        finally:
-            print ("\nEnergy in Joules = {0:.2f}\nNumber tons of TNT exploded = {1:.2f}"
-                .format(energy_in_joules, num_tons_tnt_exploded) )
-            return (energy_in_joules, num_tons_tnt_exploded)
+        except ValueError:
+            print ("Error: Invalid input, please enter a floating point number.")
+            raise
+
+        print ("\nEnergy in Joules = {0:.2f}\nNumber tons of TNT exploded = {1:.2f}"
+            .format(energy_in_joules, num_tons_tnt_exploded) )
+        return (energy_in_joules, num_tons_tnt_exploded)
