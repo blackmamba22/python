3d346a41bf58b868876ec411b4b53b7f9a44c194
Gawaine O'Gilvie <gawaine.ogilvie@gmail.com>
2016-04-12 13:56:28 -0400

    Control section: completed #1 which is to draw shapes using turtle library.

--- python3/software-eng-concepts/projects/Control.py
+++ python3/software-eng-concepts/projects/Control.py
@@ -1,4 +1,5 @@
 import sys
+import random
 
 class Control:
     """Exercises with control-flow and conditional statements."""
@@ -10,20 +11,66 @@ class Control:
     def draw_regular_patterns():
         """Draw multiple shapes and colors using turtle library."""
         import turtle
+        turtle.screensize(800, 800)
+        color_lst = ["#90C3D4", "#C390D4", "#D4A190", "#A1D490", "#92E85D", "#5DD7E8", "#B35DE8", "#E86E5D", "#34EAED", "#EBF779"]
 
-        silly = turtle.Turtle()
-        silly.color("#32D486")
-        silly.fillcolor("red")
-        x = 50
-        d = 90
+        print ("This program draws squares of many colors.\n")
+        num_squares = input("\nEnter the number of squares to draw: ")
 
-        a, b = (1,1)
-        for p in range(10):
-            silly.goto(a, b)
-            for i in range(4):
-                silly.forward(x)
-                silly.right(d)     # Rotate clockwise by 90 degrees
-            x += 25
-            a+=10
-            b+=20
+        if not num_squares.isdigit():
+            print("The number must be an integer and at least 1.\nPlease try again.")
+            sys.exit(1)
+        elif int(num_squares) < 1:
+            print("The number must be an integer and at least 1.\nPlease try again.")
+            sys.exit(1)
+
+
+        num_squares = int(num_squares)
+        turtle_man = turtle.Turtle()
+        x_direction, degree = (100, 90)
+        x_cor, y_cor = (0, 0)
+
+        for i in range(num_squares):
+            turtle_man.goto(x_cor, y_cor)
+            turtle_man.begin_fill()
+            turtle_man.down()
+            turtle_man.color(color_lst[random.randrange(0,10)])
+
+            for j in range(4):
+                turtle_man.forward(x_direction)
+                turtle_man.right(degree)
+            turtle_man.up() # pick up pen
+            turtle_man.end_fill() # end shape color fill
+            x_cor += 20
+            y_cor += 10
+        #turtle.done()
+        turtle.clear()
+
+        #======================================================================#
+        # DRAW CIRCLES
+        #======================================================================#
+        print ("This program draws circles of many colors.\n")
+        num_circles = input("\nEnter the number of circles to draw: ")
+
+        if not num_circles.isdigit():
+            print("The number must be an integer and at least 1.\nPlease try again.")
+            sys.exit(1)
+        elif int(num_circles) < 1:
+            print("The number must be an integer and at least 1.\nPlease try again.")
+            sys.exit(1)
+
+        num_circles = int(num_circles)
+        radius = 20
+        turtle_circa = turtle.Turtle()
+        w_cor, z_cor = (0, 0)
+        for i in range(num_circles):
+            turtle_circa.goto(w_cor, z_cor)
+            turtle_circa.begin_fill()
+            turtle_circa.down()
+            turtle_circa.color(color_lst[random.randrange(0,10)])
+            turtle_circa.circle(radius * 10)
+            turtle_circa.up()
+            turtle_circa.end_fill()
+            w_cor += 50
+            #z_cor += 50
         turtle.done()
--- python3/software-eng-concepts/projects/main.py
+++ python3/software-eng-concepts/projects/main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 from FirstSteps import FirstSteps as fs
+from Control import Control as cn
 
 if __name__ == '__main__':
-    print (fs.richter_scale())
+    #print (fs.richter_scale())
+    #print (fs.calculate_windchill_temperature_index())
+
+    # Control class
+    cn.draw_regular_patterns()
