5f1558b143538ccdc7194c55d94b86422fdb94b4
Gawaine O'Gilvie <gawaine.ogilvie@gmail.com>
2016-07-22 16:14:58 -0400

    Completed FizzBuzz

--- mega_project_list/projects/fizz_buzz.py
+++ mega_project_list/projects/fizz_buzz.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 """Write a program that prints the numbers from 1 to 100.
@@ -5,5 +6,24 @@ But for multiples of three print “Fizz” instead of the number and for the
 multiples of five print “Buzz”. For numbers which are multiples of both three
 and five print “FizzBuzz”."""
 
+
 def fizz_buzz():
-    
+    """FizzBuzz function"""
+    output = []
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", i)
+            output.append("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz", i)
+            output.append("Fizz")
+        elif i % 5 == 0:
+            print("Buzz", i)
+            output.append("Buzz")
+        else:
+            print(i)
+            output.append(i)
+
+
+if __name__ == '__main__':
+    fizz_buzz()
