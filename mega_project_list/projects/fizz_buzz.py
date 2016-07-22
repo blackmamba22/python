a8911d647b1d915f131c1dd9b5ed3259f6e26385
Gawaine O'Gilvie <gawaine.ogilvie@gmail.com>
2016-07-22 16:19:12 -0400

    Completed FizzBuzz

--- mega_project_list/projects/fizz_buzz.py
+++ mega_project_list/projects/fizz_buzz.py
@@ -9,20 +9,15 @@ and five print “FizzBuzz”."""
 
 def fizz_buzz():
     """FizzBuzz function"""
-    output = []
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", i)
-            output.append("FizzBuzz")
         elif i % 3 == 0:
             print("Fizz", i)
-            output.append("Fizz")
         elif i % 5 == 0:
             print("Buzz", i)
-            output.append("Buzz")
         else:
             print(i)
-            output.append(i)
 
 
 if __name__ == '__main__':
